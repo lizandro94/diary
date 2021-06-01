100580d84d700e09eaabd9584139686c5f3a5a8d
Leandro Fonseca <leandrofonsecaalfaro@hotmail.com>
2021-05-31 20:15:46 -0600

    Add domain entities

---
+++ Diary.Domain/Entities/Diary.cs
@@ -0,0 +1,16 @@
+﻿using Diary.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diary.Domain.Entities
+{
+    public class Diary : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Memory> Memories { get; set; }
+    }
+}
---
+++ Diary.Domain/Entities/Memory.cs
@@ -0,0 +1,17 @@
+﻿using Diary.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diary.Domain.Entities
+{
+    public class Memory : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Details { get; set; }
+        public Guid DiaryId { get; set; }
+        public Diary Diary { get; set; }
+    }
+}
