01a9cd9e573497718e5162d5fd3077a4752a6f1c
Leandro Fonseca <leandrofonsecaalfaro@hotmail.com>
2021-06-07 19:36:17 -0600

    Add diary repo

---
+++ Diary.Persistence/Repositories/DiaryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Diary.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diary.Persistence.Repositories
+{
+    public class DiaryRepository : BaseRepository<Domain.Entities.Diary>, IDiaryRepository
+    {
+        public DiaryRepository(DiaryDbContext dbContext) : base(dbContext)
+        {
+        }
+        public async Task<List<Domain.Entities.Diary>> GetDiariesByUserId(Guid userId)
+        {
+            var diaries = await _dbContext.Diaries.Where(d => d.UserId == userId).ToListAsync();
+            return diaries;
+        }
+    }
+}
